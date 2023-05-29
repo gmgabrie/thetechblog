791e018b069043808b4f8add6b67e3a1e171a6a2
gmgabrie <ggabriel420@gmail.com>
2023-05-29 15:52:40 -0400

    finished base login and signup pages and verified functionality

---
+++ controllers/home-routes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const Posts = require("../models");
+
+// GET all posts for homepage
+router.get("/", async (req, res) => {
+  try {
+    // const dbPostData = await Posts.findAll({
+    //   include: [
+    //     {
+    //       model: Posts,
+    //       attributes: ["title", "content"],
+    //     },
+    //   ],
+    // });
+
+    // const posts = dbPostData.map((posts) => posts.get({ plain: true }));
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    // res.render("homepage", {
+    //   posts,
+    //   loggedIn: req.session.loggedIn,
+    // });
+    res.render("homepage");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// // GET one gallery
+// router.get('/gallery/:id', async (req, res) => {
+//   try {
+//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Painting,
+//           attributes: [
+//             'id',
+//             'title',
+//             'artist',
+//             'exhibition_date',
+//             'filename',
+//             'description',
+//           ],
+//         },
+//       ],
+//     });
+
+//     const gallery = dbGalleryData.get({ plain: true });
+//     // Send over the 'loggedIn' session variable to the 'gallery' template
+//     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// // GET one painting
+// router.get('/painting/:id', async (req, res) => {
+//   try {
+//     const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//     const painting = dbPaintingData.get({ plain: true });
+//     // Send over the 'loggedIn' session variable to the 'homepage' template
+//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// Login route
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render("login");
+});
+
+//Signup route
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'signup' page
+  res.render("signup");
+});
+
+module.exports = router;
